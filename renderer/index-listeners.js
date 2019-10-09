9b70f99b72c14ea8347f6f940445ada04b0e7121
MDrealan <markus-drealan@uiowa.edu>
2019-10-08 21:25:54 -0500

    everythings a mess but it works - hilbert transform moves with filtered data. scaling currently is an issue as it freezes the app.

--- renderer/index-listeners.js
+++ renderer/index-listeners.js
@@ -8,11 +8,13 @@ var dataPath = 'pyfolder/zorin_testData080511_oneElec.mat'; //path to folder.
 
 var filteredData;
 var filteredDataArr = []; //handling large amounts of data - the python script can only push a certain amount of data to node at a time (~arr size 3288), so handle that by pushing to array.
+var scaledFilteredDataArr = [];
 var filteredDataDiv;
 var x_axis_time;
 var x_axis_time_arr = []; //same as ^
 var Fs = 1000 //Todo:: get from user.
 
+var hilbertPhaseData = []
 var processedDataHolder = []; // array of arrays, each array is like filteredData.
 var processedDataDivs = []; //corresponding divs of processedDataHolder divs.
 var iterator_position = 0; // keep track of where we are.
@@ -32,49 +34,51 @@ function loadData()
         var string_arr = output_data.toString()
         string_arr = string_arr.split(",")
         filteredData = string_arr.map(Number)
-        for( i = 0; i < filteredData.length; i++)
+        for( var i = 0; i < filteredData.length; i++)
         {
             filteredData[i] = filteredData[i] || 0
             filteredData[0] = 0 //WTF python-shell???
         }
         filteredDataArr.push(filteredData)
-        x_axis_time = Array(filteredData.length).keys(); //Todo:: this is incorrect.
-        x_axis_time_arr.push(x_axis_time)
     });
     pyshell.end(function (err,code,signal) 
     {
+        var overall_iterator = 0;
+        //create x axis time for data now that we've processed all the input data.
+        for( var i = 0; i < filteredDataArr.length; i++)
+        {
+            var curr_x_axis_time = []
+            for (var j = 0; j < filteredDataArr[i].length; j++)
+            {
+                curr_x_axis_time.push(overall_iterator);
+                overall_iterator = overall_iterator + 1
+            }
+            x_axis_time_arr.push(curr_x_axis_time)
+        }
+
         filteredDataDiv = document.getElementById("filterDivPlot");
-        graphData(filteredDataDiv, x_axis_time[0],filteredDataArr[0], 'Filtered Data', 'Time (s)', 'Amplitude' )
+        graphData(filteredDataDiv, x_axis_time_arr[0],filteredDataArr[0], 'Filtered Data', 'Time (s)', 'Amplitude' )
 
         // create the x axis iterator for the plots.
         createXIterator();
+       // scaleFilteredData();
     });
 }
 
-function hilbertTransform()
+function scaleFilteredData()
 {
-    // scale(number, oldMin, oldMax, newMin, newMax);
-    filtered_scaled = []
-    max_filtered = Math.max(...filteredData)
-    min_filtered = Math.min(...filteredData)
-    for( i = 0; i < filteredData.length; i++) //Scale to -180, 180 so you can see instant phase better.
-    {
-        filtered_scaled.push( scale(filteredData[i], min_filtered, max_filtered, -180, 180))
+    for( var i = 0 ; i < filteredDataArr.length; i++)
+    { 
+        max_filtered = Math.max(...filteredDataArr[i])
+        min_filtered = Math.min(...filteredDataArr[i])
+        var curr_scaled = []
+        var curr_unscaled = filteredDataArr[i]
+        for( var j = 0 ; i < filteredDataArr[i].length ; j++)
+        {
+            curr_scaled.push( scale(curr_unscaled[j], min_filtered, max_filtered, -180, 180 ))
+        }
+        scaledFilteredDataArr.push(curr_arr)
     }
-    let inputOptions = {
-        mode: 'binary',
-        pythonOptions: ['-u'],
-        args: [ filteredData ]
-    }
-    var py = new PythonShell('pyfolder/hilbertTransform.py', inputOptions)
-    py.stdout.on('data', function (data) {
-        dataString = data.toString().split(",")
-        hilbertInstantPhase = dataString.map(Number)
-        var hilbert_div = document.getElementById("hilbertDivPlot");
-        twoPlotHilbertX = [x_axis_time, x_axis_time]
-        twoPlotHilbertY = [hilbertInstantPhase, filtered_scaled]
-        graphData(hilbert_div, twoPlotHilbertX, twoPlotHilbertY, 'Instantaneous Phase: Hilbert Transform', 'Time (s)', 'Phase & Scaled Amplitude', twoPlot=true )
-    });
 }
   
 function graphData(div, data_x, data_y, title, x_title, y_title, twoPlot =false)
@@ -126,31 +130,33 @@ function createXIterator()
     backwardIterator.removeAttribute("hidden");
 
     forwardIterator.addEventListener('click', () => {
-        if( (iterator_position+1) >= filteredData.length)
-        {
-
-        }
+        if( (iterator_position+1) >= filteredDataArr.length) { }
         else
         {
             iterator_position = iterator_position + 1
-            graphData(filteredDataDiv, x_axis_time[iterator_position],filteredDataArr[iterator_position], 'Filtered Data', 'Time (s)', 'Amplitude' )
+            graphData(filteredDataDiv, x_axis_time_arr[iterator_position],filteredDataArr[iterator_position], 'Filtered Data', 'Time (s)', 'Amplitude' )
+            batchGraph(iterator_position)
         }
     })
     backwardIterator.addEventListener('click', ()=> {
-        if( (iterator_position - 1) <= 0)
-        {
-
-        }
+        if( (iterator_position - 1) < 0) { }
         else
         {
             iterator_position = iterator_position - 1
-            graphData(filteredDataDiv, x_axis_time[iterator_position],filteredDataArr[iterator_position], 'Filtered Data', 'Time (s)', 'Amplitude' )
+            graphData(filteredDataDiv, x_axis_time_arr[iterator_position],filteredDataArr[iterator_position], 'Filtered Data', 'Time (s)', 'Amplitude' )
+            batchGraph(iterator_position)
         }
     })
 }
-function iterate_plot()
-{
 
+function batchGraph(iterator_position)
+{
+    for (var i = 0; i < processedDataDivs.length; i++)
+    {
+        var twoPlotX = [x_axis_time_arr[iterator_position], x_axis_time_arr[iterator_position]]
+        var twoPlotY = [hilbertPhaseData[iterator_position], filteredDataArr[iterator_position]]
+        graphData(processedDataDivs[i], twoPlotX, twoPlotY, 'Instantaneous Phase: Hilbert Transform', 'Time ', 'Phase & Scaled Amplitude', twoPlot=true )
+    }
 }
 
 function getFolder() 
@@ -160,6 +166,66 @@ function getFolder()
     });
     console.log(folderPath.toString())
 }
+
+
+
+
+
+
+function hilbertTransform()
+{
+    let inputOptions = {
+        mode: 'binary',
+        pythonOptions: ['-u'],
+        args: [ filteredDataArr[0] ]
+    }
+    var py = new PythonShell('pyfolder/hilbertTransform.py', inputOptions)
+    py.stdout.on('data', function (output_data) 
+    {
+        var string_arr = output_data.toString().split(",")
+        hilbertPhase = string_arr.map(Number)
+        for( var i = 0; i < hilbertPhase.length; i++)
+        {
+            hilbertPhase[i] = hilbertPhase[i] || 0
+            hilbertPhase[0] = 0
+        }
+        hilbertPhaseData.push(hilbertPhase)
+    });
+    py.end(function (err,code,signal) 
+    {
+        var hilbert_div = document.getElementById("hilbertDivPlot");
+        processedDataDivs.push(hilbert_div)
+        twoPlotHilbertX = [x_axis_time_arr[0], x_axis_time_arr[0]]
+        twoPlotHilbertY = [hilbertPhaseData[0], filteredDataArr[0]]
+        graphData(hilbert_div, twoPlotHilbertX, twoPlotHilbertY, 'Instantaneous Phase: Hilbert Transform', 'Time ', 'Phase & Scaled Amplitude', twoPlot=true )
+    });
+    for(var i = 1; i < filteredDataArr.length; i++)
+    {
+    processRemainingData(filteredDataArr[i])
+    }
+}
+
+function processRemainingData(data_input)
+{
+    let inputOptions = {
+        mode: 'binary',
+        pythonOptions: ['-u'],
+        args: [ data_input ]
+    }
+    var py = new PythonShell('pyfolder/hilbertTransform.py', inputOptions)
+    py.stdout.on('data', function (output_data) 
+    {
+        var string_arr = output_data.toString().split(",")
+        hilbertPhase = string_arr.map(Number)
+        for( var i = 0; i < hilbertPhase.length; i++)
+        {
+            hilbertPhase[i] = hilbertPhase[i] || 0
+            hilbertPhase[0] = 0
+        }
+        hilbertPhaseData.push(hilbertPhase)
+    });
+}
+
 
 function createListeners()
 {
